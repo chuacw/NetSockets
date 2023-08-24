ab50c5e700ecf559051b1e81581adc7b25ecd16b
chuacw <1757930+chuacw@users.noreply.github.com>
2023-08-24 23:18:56 +0800

    Updated project files

--- Client/NetSocket.Client.dpr
+++ Client/NetSocket.Client.dpr
@@ -3,7 +3,8 @@ program NetSocket.Client;
 uses
   Vcl.Forms,
   NetSocket.Client.Main in 'NetSocket.Client.Main.pas' {frmSocketClient},
-  System.Net.ClientSocket in 'System.Net.ClientSocket.pas';
+  System.Net.ClientSocket in 'System.Net.ClientSocket.pas',
+  System.Net.Socket.Common in '..\Common\System.Net.Socket.Common.pas';
 
 {$R *.res}
 
--- Server/NetSocket.Server.dpr
+++ Server/NetSocket.Server.dpr
@@ -3,7 +3,8 @@ program NetSocket.Server;
 uses
   Vcl.Forms,
   NetSocket.Server.Main in 'NetSocket.Server.Main.pas' {frmSocketServer},
-  System.Net.ServerSocket in 'System.Net.ServerSocket.pas';
+  System.Net.ServerSocket in 'System.Net.ServerSocket.pas',
+  System.Net.Socket.Common in '..\Common\System.Net.Socket.Common.pas';
 
 {$R *.res}
 
